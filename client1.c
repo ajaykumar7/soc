20017a99ba531447d378b5cc22451e0af3bda5de
ajaykumar7 <yadavajayk888@gmail.com>
2019-12-02 20:03:56 +0530

    ser/cli

--- client1.c
+++ client1.c
@@ -3,22 +3,57 @@
 # include<netinet/in.h>
 # include<strings.h>
 # include<string.h>
+// Global Data
+void send_soc(int sockfd,char * buf);
+void recv_soc(int sockfd,char * buf);
+ int flag = 2;
+
 int main(int argc,char **argv)
 {
  int sockfd,n;
 char buf[4096];  
+char buf2[4096];  
+
 struct sockaddr_in cliaddr;
 sockfd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 bzero(&cliaddr,sizeof(cliaddr));
 cliaddr.sin_family=AF_INET;
-cliaddr.sin_port=htons(9888);
-cliaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+cliaddr.sin_port=htons(8082);
+//cliaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+inet_pton(AF_INET, argv[1], &cliaddr.sin_addr);
 connect(sockfd,(struct sockaddr *) &cliaddr,sizeof(cliaddr));
-printf("enter a string\n");
+    printf("------------------------------------------------------\n");
+
+    printf("Connection Established \n");
+
+    printf("-------------WELCOME TO COMMAND CHATTING----------------------\n");
+    
+while (1) {
+if (flag == 2)	
+{
+memset(buf , 0 , sizeof(buf));	
 fgets(buf,256,stdin);
 write(sockfd,buf,strlen(buf));
-n=read(sockfd,buf,256);
-printf("\n\nreceived message from server:%s\n",buf);
+send_soc(sockfd,&buf);
+}
+if (flag == 1)
+{
+recv_soc(sockfd,&buf2);	
+}
+}
 close(sockfd);
 }
 
+void send_soc(int sockfd,char * buf)
+{
+send(sockfd,&buf,strlen(buf),0);
+printf("You-: %s", buf);
+flag =1;
+}
+
+void recv_soc(int sockfd,char * buf2)
+{
+recv(sockfd, buf2, sizeof(buf2),0);
+printf("Him-: %s", buf2);
+flag =2;
+}
--- server1.c
+++ server1.c
@@ -4,25 +4,55 @@
 # include<strings.h>
 # include<string.h>
 
+void send_soc(int sockfd,char * buf);
+void recv_soc(int sockfd,char * buf);
+int flag = 2;
 int main(int argc,char **argv)
 {
  int i,j,l,sockfd,clilen,newsockfd,n;
-char buf[4096],buf1[4096]="keep it coming.......\0";
+char buf[4096],buf1[4096];
 
 struct sockaddr_in servaddr,cliaddr;
 sockfd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 bzero(&servaddr,sizeof(servaddr));
 servaddr.sin_family=AF_INET;
-servaddr.sin_port=htons(9888);
+servaddr.sin_port=htons(8082);
 servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+
 bind(sockfd,(struct aockaddr *)&servaddr,sizeof(servaddr));
 listen(sockfd,5);
 clilen=sizeof(cliaddr);
 newsockfd=accept(sockfd,(struct sockaddr *) &cliaddr,&clilen);
-n=read(newsockfd,buf,4000);
-printf("\n\nreceived from client:%s\n",buf);
-l=strlen(buf);
-write(newsockfd,buf1,strlen(buf1));
+    printf("------------------------------------------------------\n");
+
+    printf("Connection Established with Client \n");
+
+    printf("-------------WELCOME TO COMMAND CHATTING----------------------\n");
+while (1) {
+if (flag == 1)	
+{
+fgets(buf,256,stdin);
+write(newsockfd,buf,strlen(buf));
+send_soc(newsockfd,&buf);
+}
+if (flag == 2)
+{
+recv_soc(newsockfd,&buf1);	
+}
+}
 close(newsockfd);
 }
 
+void send_soc(int newsockfd,char * buf)
+{
+send(newsockfd,&buf, strlen(buf),0);
+printf("You-: %s", buf);
+flag =2;
+}
+
+void recv_soc(int newsockfd,char * buf1)
+{
+recv(newsockfd, buf1, sizeof(buf1),0);	
+printf("Him-: %s", buf1);
+flag =1;
+}
